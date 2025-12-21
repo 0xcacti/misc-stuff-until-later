cb39c37305e36c507f26adc34e72f7960ed51c47
0xcacti <0xcacti@gmail.com>
2025-12-21 02:06:43 -0800

    add exit codes

--- cat/main.c
+++ cat/main.c
@@ -1,3 +1,5 @@
+#define _POSIX_C_SOURCE 200809L
+
 #include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
@@ -7,10 +9,15 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-void usage() { fprintf(stderr, "Usage: mcat [file]\n"); }
+static void usage() {
+  fprintf(stderr, "Usage: mcat [file]\n");
+}
 
-void error_msg(const char *filename) {
+static void error_msg(const char *filename) {
   fprintf(stderr, "mcat: %s: %s\n", filename, strerror(errno));
+}
+
+static void write_all(int fd, const void *buf, size_t len) {
 }
 
 int main(int argc, char *argv[]) {
@@ -25,32 +32,51 @@ int main(int argc, char *argv[]) {
     }
   }
 
+  int exit_code = 0;
   for (int i = optind; i < argc; i++) {
     char *filename = argv[i];
     int fd = open(filename, O_RDONLY);
     if (fd < 0) {
-      if (errno == EACCES) {
-        error_msg(filename);
-      }
+      error_msg(filename);
+      continue;
     }
 
     struct stat st;
     if (fstat(fd, &st) < 0) {
       error_msg(filename);
       close(fd);
+      exit_code = 1;
       continue;
     }
     size_t sz = st.st_size;
+    if (sz == 0) {
+      close(fd);
+      exit_code = 1;
+      continue;
+    }
+
     void *p = mmap(0, sz, PROT_READ, MAP_PRIVATE, fd, 0);
     if (p == MAP_FAILED) {
       error_msg(filename);
       close(fd);
+      exit_code = 1;
       continue;
     }
 
-    write(0, p, sz);
+    if (write(1, p, sz) < 0) {
+      error_msg(filename);
+      close(fd);
+      exit_code = 1;
+      continue;
+    }
 
-    // TODO: error
-    munmap(p, sz);
+    if (munmap(p, sz) < 0) {
+      error_msg(filename);
+      close(fd);
+      exit_code = 1;
+      continue;
+    }
+    close(fd);
   }
+  return exit_code;
 }
